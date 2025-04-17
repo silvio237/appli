b3b9d342fc5eeaee0d981b81abdaf3ae57c89c60
silvio <silviongougni139@gmail.com>
2025-04-17 16:30:28 +0200

    Refactored API to fix build issue

--- app/api/companies/route.ts
+++ app/api/companies/route.ts
@@ -2,47 +2,37 @@ import { NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 
 export async function POST(request: Request) {
-
     try {
-        const { email, companyName } = await request.json()
+        const { email, companyName } = await request.json();
 
         if (!email || !companyName) {
-            return NextResponse.json({ error: 'Email et nom de l\'entreprise sont requis.' },
-                { status: 400 });
+            return NextResponse.json({ error: 'Email et nom de l\'entreprise sont requis.' }, { status: 400 });
         }
 
         const user = await prisma.user.findUnique({
             where: { email }
-        })
+        });
 
         if (!user) {
-            return NextResponse.json({ message: 'Utilisateur non trouvé.' },
-                { status: 404 });
+            return NextResponse.json({ message: 'Utilisateur non trouvé.' }, { status: 404 });
         }
 
         const existingCompany = await prisma.company.findUnique({
             where: { name: companyName }
-        })
+        });
 
         if (existingCompany) {
-            return NextResponse.json(
-                { message: 'Une entreprise avec ce nom existe déjà.' },
-                { status: 409 }
-            );
+            return NextResponse.json({ message: 'Une entreprise avec ce nom existe déjà.' }, { status: 409 });
         }
-
 
         const newCompany = await prisma.company.create({
             data: {
                 name: companyName,
                 createdBy: { connect: { id: user.id } },
             }
-        })
+        });
 
-        return NextResponse.json(
-            { message: 'Entreprise créée avec succès.', company: newCompany },
-            { status: 201 }
-        );
+        return NextResponse.json({ message: 'Entreprise créée avec succès.', company: newCompany }, { status: 201 });
 
     } catch (error) {
         console.error('Error in API:', error);
@@ -52,8 +42,8 @@ export async function POST(request: Request) {
 
 export async function GET(request: Request) {
     try {
-        const { searchParams } = new URL(request.url)
-        const email = searchParams.get('email')
+        const { searchParams } = new URL(request.url);
+        const email = searchParams.get('email');
 
         if (!email) {
             return NextResponse.json({ error: 'L\'email est requis.' }, { status: 400 });
@@ -61,7 +51,7 @@ export async function GET(request: Request) {
 
         const user = await prisma.user.findUnique({
             where: { email }
-        })
+        });
 
         if (!user) {
             return NextResponse.json({ error: 'Utilisateur non trouvé.' }, { status: 404 });
@@ -71,11 +61,9 @@ export async function GET(request: Request) {
             where: {
                 createdById: user.id
             }
-        })
+        });
 
-        return NextResponse.json(
-            { companies },
-            { status: 200 });
+        return NextResponse.json({ companies }, { status: 200 });
 
     } catch (error) {
         console.error('Error in API:', error);
@@ -89,7 +77,7 @@ export async function DELETE(request: Request) {
 
         const company = await prisma.company.findUnique({
             where: { id }
-        })
+        });
 
         if (!company) {
             return NextResponse.json({ message: 'Entreprise non trouvée' }, { status: 404 });
@@ -98,7 +86,7 @@ export async function DELETE(request: Request) {
         await prisma.user.updateMany({
             where: { companyId: id },
             data: { companyId: null }
-        })
+        });
 
         await prisma.reservation.deleteMany({
             where: {
@@ -106,19 +94,20 @@ export async function DELETE(request: Request) {
                     companyId: id
                 }
             }
-        })
+        });
 
         await prisma.room.deleteMany({
             where: {
                 companyId: id,
             }
-        })
+        });
 
         await prisma.company.delete({
             where: { id }
-        })
+        });
 
         return NextResponse.json({ message: 'Entreprise supprimée avec succès' }, { status: 200 });
+
     } catch (error) {
         console.error('Error in API:', error);
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
@@ -127,11 +116,11 @@ export async function DELETE(request: Request) {
 
 export async function PATCH(request: Request) {
     try {
-        const { id, creatorEmail, employeeEmail, action } = await request.json()
+        const { id, creatorEmail, employeeEmail, action } = await request.json();
 
         const creator = await prisma.user.findUnique({
             where: { email: creatorEmail }
-        })
+        });
 
         if (!creator) {
             return NextResponse.json({ message: 'Créateur non trouvé' }, { status: 404 });
@@ -139,7 +128,7 @@ export async function PATCH(request: Request) {
 
         const company = await prisma.company.findUnique({
             where: { id }
-        })
+        });
 
         if (!company) {
             return NextResponse.json({ message: 'Entreprise non trouvée' }, { status: 404 });
@@ -152,7 +141,7 @@ export async function PATCH(request: Request) {
         if (action === 'ADD') {
             const employee = await prisma.user.findUnique({
                 where: { email: employeeEmail }
-            })
+            });
 
             if (employee?.companyId === company.id) {
                 return NextResponse.json({ message: `${employeeEmail} est déjà dans l'entreprise` }, { status: 400 });
@@ -168,14 +157,12 @@ export async function PATCH(request: Request) {
                         email: employeeEmail,
                         companyId: company.id
                     }
-                })
+                });
             } else {
                 await prisma.user.update({
                     where: { id: employee.id },
-                    data: {
-                        companyId: company.id
-                    }
-                })
+                    data: { companyId: company.id }
+                });
             }
 
             await prisma.company.update({
@@ -185,14 +172,14 @@ export async function PATCH(request: Request) {
                         connect: { id: employee!.id }
                     }
                 }
-            })
+            });
 
             return NextResponse.json({ message: 'Employé ajouté avec succès' }, { status: 201 });
 
         } else if (action === 'DELETE') {
             const employee = await prisma.user.findUnique({
                 where: { email: employeeEmail }
-            })
+            });
 
             if (!employee) {
                 return NextResponse.json({ message: 'Employé non trouvé' }, { status: 404 });
@@ -205,11 +192,12 @@ export async function PATCH(request: Request) {
                         disconnect: { id: employee.id }
                     }
                 }
-            })
+            });
 
             return NextResponse.json({ message: 'Employé supprimé avec succès' }, { status: 200 });
 
         }
+
     } catch (error) {
         console.error('Error in API:', error);
         return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
