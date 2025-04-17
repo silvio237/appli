da100510c98500ebc6b3870f7c0e50490e85c1ac
silvio <silviongougni139@gmail.com>
2025-04-17 16:19:14 +0200

    Fix cleanupReservations API

--- app/api/cleanupReservations/route.ts
+++ app/api/cleanupReservations/route.ts
@@ -3,45 +3,48 @@ import prisma from '@/lib/prisma';
 import dayjs from 'dayjs';
 
 export async function DELETE() {
-    try {
-        const now = dayjs()
+  try {
+    // Utilisation de dayjs pour récupérer l'heure actuelle
+    const now = dayjs();
 
-        const expiredReservations = await prisma.reservation.findMany({
-            where : {
-                OR: [
-                    {
-                        reservationDate : {
-                            lt:now.format('DD/MM/YYYY')
-                        }
-                    },
-                    {
-                        reservationDate : {
-                            equals:now.format('DD/MM/YYYY')
-                        },
-                        endTime : {
-                            lt: now.format('HH:mm')
-                        }
+    // Recherche des réservations expirées
+    const expiredReservations = await prisma.reservation.findMany({
+      where: {
+        OR: [
+          {
+            reservationDate: {
+              lt: now.format('DD/MM/YYYY'), // Si la date de réservation est inférieure à la date actuelle
+            },
+          },
+          {
+            reservationDate: {
+              equals: now.format('DD/MM/YYYY'), // Si la date de réservation est égale à la date actuelle
+            },
+            endTime: {
+              lt: now.format('HH:mm'), // Si l'heure de fin est inférieure à l'heure actuelle
+            },
+          },
+        ],
+      },
+    });
 
-                    }
-             
-                ]
-            }
-        })
+    // Si des réservations expirées sont trouvées, on les supprime
+    if (expiredReservations.length > 0) {
+      await prisma.reservation.deleteMany({
+        where: {
+          id: {
+            in: expiredReservations.map((reservation) => reservation.id),
+          },
+        },
+      });
+    }
 
-        if(expiredReservations.length > 0){
-            await prisma.reservation.deleteMany({
-                where : {
-                    id : {
-                        in : expiredReservations.map((reservation) => reservation.id)
-                    }
-                }
-            })
-        }
-        
+    // Retourne une réponse positive indiquant que les réservations expirées ont été nettoyées
     return NextResponse.json({ message: 'Expired reservations cleaned up' });
- 
-    } catch (error) {
-        console.error('Error cleaning up reservations:', error);
-        return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
-      }
+
+  } catch (error) {
+    // En cas d'erreur, on logge l'erreur et on retourne un message d'erreur avec un code 500
+    console.error('Error cleaning up reservations:', error);
+    return NextResponse.json({ message: 'Internal server error' }, { status: 500 });
+  }
 }
